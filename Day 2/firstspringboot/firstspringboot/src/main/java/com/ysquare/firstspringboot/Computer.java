f717c2c43baf9cd54eef87cc99a8a995353d5726
yyogesh <yogeshggn@hotmail.com>
2024-02-19 09:13:33 +0530

    Spring boot

---
+++ Day 2/firstspringboot/firstspringboot/src/main/java/com/ysquare/firstspringboot/Computer.java
@@ -0,0 +1,5 @@
+package com.ysquare.firstspringboot;
+
+public interface Computer {
+    void compile();
+}
---
+++ Day 2/firstspringboot/firstspringboot/src/main/java/com/ysquare/firstspringboot/Desktop.java
@@ -0,0 +1,14 @@
+package com.ysquare.firstspringboot;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class Desktop implements Computer{
+
+    @Override
+    public void compile() {
+        System.out.println("compiling on Desktop");
+    }
+}
